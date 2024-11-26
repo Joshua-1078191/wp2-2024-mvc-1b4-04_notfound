2522c4eae8a1397e568ac4e5f300494514c5a44c
jorik-1099493 <1099493@hr.nl>
2024-11-26 16:14:56 +0100

    added post methods to index and import

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 def main():
     return render_template("index-1.html")
 
-@app.route('/import')
+@app.route('/import', methods=['GET', 'POST'])
 def import_questions():
     return render_template("questions/import_questions.html")
 
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index_questions():
     return render_template("questions/index_questions.html")
