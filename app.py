a8296887ab0e51118a259f7b6c19486a7541dee3
Roos-1104659 <1104659@hr.nl>
2024-12-08 13:38:19 +0100

    made redacteur_wijzigen.html.jinja clean with jinja

--- app.py
+++ app.py
@@ -20,15 +20,15 @@ def index_questions_prompt():
 def index_questions_taxonomy(prompt_id:int):
     return render_template("questions/index_questions_taxonomy.html.jinja")
 
-@app.route('/index/lijst_redacteuren', methods=['GET', 'POST'])
+@app.route('/lijst_redacteuren', methods=['GET', 'POST'])
 def lijst_redacteuren():
     return render_template("lijst_redacteuren.html.jinja")
 
-@app.route('/index/nieuwe_redacteur', methods=['GET', 'POST'])
+@app.route('/nieuwe_redacteur', methods=['GET', 'POST'])
 def nieuwe_redacteuren():
     return render_template("nieuwe_redacteur.html.jinja")
 
-@app.route('/index/redacteur_wijzigen', methods=['GET', 'POST'])
+@app.route('/redacteur_wijzigen', methods=['GET', 'POST'])
 def redacteur_wijzigen():
     return render_template("redacteur_wijzigen.html.jinja")
 
