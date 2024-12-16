2a68edfe5a44ed08cf24b1a36f0d96666f280c5e
Roos-1104659 <1104659@hr.nl>
2024-12-16 21:44:14 +0100

    added function to see all prompts

--- src/models/prompts.py
+++ src/models/prompts.py
@@ -1,12 +1,20 @@
 import sqlite3
 
+
 class Prompts:
     def __init__(self, db_path):
         self.db = db_path
 
-    #Voor Roos: Hier moet jouw functie komen te staan!
+    def prompt_all_view(self):
+        con = sqlite3.connect(self.db)
+        cursor = con.cursor()
 
-    def get_one_prompt(self, prompt_id:int):
+        cursor.execute("""SELECT * FROM prompts""")
+        con.commit()
+
+        return cursor.fetchall()
+
+    def get_one_prompt(self, prompt_id: int):
         con = sqlite3.connect(self.db)
 
         cur = con.cursor()
@@ -15,24 +23,23 @@ class Prompts:
         SELECT * FROM prompts WHERE prompts_id = ? LIMIT 1;
         """, (prompt_id,)).fetchone()
 
-
         if not prompt_data:
             return None
 
         result = {
-            "prompt_id" : prompt_data["prompts_id"],
-            "redacteur" : prompt_data["user_id"],
-            "prompt_naam" : prompt_data["prompt"],
-            "categorised_questions" : prompt_data["questions_count"],
-            "correct_questions" : prompt_data["questions_correct"],
-            "creation_date" : prompt_data["date_created"],
+            "prompt_id": prompt_data["prompts_id"],
+            "redacteur": prompt_data["user_id"],
+            "prompt_naam": prompt_data["prompt"],
+            "categorised_questions": prompt_data["questions_count"],
+            "correct_questions": prompt_data["questions_correct"],
+            "creation_date": prompt_data["date_created"],
         }
 
         cur.close()
 
         return result
 
-    def add_prompt(self, user_id:int, prompt:str, questions_count:int, questions_correct:int):
+    def add_prompt(self, user_id: int, prompt: str, questions_count: int, questions_correct: int):
         con = sqlite3.connect(self.db)
 
         cur = con.cursor()
