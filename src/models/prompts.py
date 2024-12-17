40fe665d0e16f4130ff921cb378916e27cdfad4e
jorik-1099493 <1099493@hr.nl>
2024-12-17 16:10:11 +0100

    Fixed prompts model

--- src/models/prompts.py
+++ src/models/prompts.py
@@ -7,18 +7,19 @@ class Prompts:
 
     def prompt_all_view(self):
         con = sqlite3.connect(self.db)
-        cursor = con.cursor()
         con.row_factory = sqlite3.Row
+        cursor = con.cursor()
 
         prompt_all_data = cursor.execute("""SELECT * FROM prompts""").fetchall()
 
         if not prompt_all_data:
-            return None
+            return []
 
         result = [{
-            "prompt_id": prompt_all["prompts_id"],
+            "id": prompt_all["prompts_id"],
             "redacteur": prompt_all["user_id"],
-            "prompt_naam": prompt_all["prompt"],
+            "name": prompt_all["prompt_name"],
+            "prompt": prompt_all["prompt"],
             "categorised_questions": prompt_all["questions_count"],
             "correct_questions": prompt_all["questions_correct"],
             "creation_date": prompt_all["date_created"]
@@ -32,7 +33,7 @@ class Prompts:
 
     def get_one_prompt(self, prompt_id: int):
         con = sqlite3.connect(self.db)
-
+        con.row_factory = sqlite3.Row
         cur = con.cursor()
 
         prompt_data = cur.execute("""
@@ -55,17 +56,17 @@ class Prompts:
 
         return result
 
-    def add_prompt(self, user_id: int, prompt: str, questions_count: int, questions_correct: int):
+    def add_prompt(self, user_id: int, prompt_name: str, prompt: str, questions_count: int, questions_correct: int):
         con = sqlite3.connect(self.db)
-
+        con.row_factory = sqlite3.Row
         cur = con.cursor()
 
         cur.execute(
             """
-            INSERT INTO prompts (user_id, prompt, questions_count, questions_correct)
-            VALUES (?, ?, ?, ?)
+            INSERT INTO prompts (user_id, prompt_name, prompt, questions_count, questions_correct)
+            VALUES (?, ?, ?, ?, ?)
             """,
-            (user_id, prompt, questions_count, questions_correct)
+            (user_id, prompt_name, prompt, questions_count, questions_correct)
         )
 
         con.commit()
